328a29da84a05dd55a0ceace39c88f50c8c3ef75
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2026-05-27 19:57:37 +0330

    initial commit

---
+++ src/app/student/courses/page.tsx
@@ -0,0 +1,3 @@
+export default async function courses() {
+  return <h1>This is student courses page</h1>;
+}
---
+++ src/app/student/profile/page.tsx
@@ -0,0 +1,3 @@
+export default async function Profile() {
+  return <h1>this is proff</h1>;
+}

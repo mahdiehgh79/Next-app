778d981f842adf7a4f9169797eec8d53078ba014
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2026-05-28 13:06:46 +0330

    add back deleted blog slug page

---
+++ src/app/(auth)/signin/verify/page.tsx
@@ -0,0 +1,7 @@
+export default async function Verify() {
+  return (
+    <div className="text-5xl flex justify-center items-center w-full">
+      <h1>this is verify page</h1>
+    </div>
+  );
+}
---
+++ src/app/(blog)/blog/[slug]/page.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default async function BlogDetailsPage({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+
+  return (
+    <div className="text-5xl flex justify-center items-center w-full">
+      <h1>{slug}</h1>
+    </div>
+  );
+}

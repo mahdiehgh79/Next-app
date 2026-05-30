113316adcf9014a0db3194dd1df2bf49995e4786
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2026-05-30 16:45:27 +0330

    some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,61 @@
 import "./globals.css";
 
+import { Figtree } from "next/font/google";
+// import localFont from "next/font/local";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-figtree",
+  weight: ["300", "400", "500", "600", "700", "800", "900"],
+});
+
+// const yekanbakh = localFont({
+//   src: [
+//       {
+//           path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Thin.woff2",
+//           weight: "100",
+//           style: "normal",
+//       },
+//       {
+//           path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Light.woff2",
+//           weight: "300",
+//           style: "normal",
+//       },
+//       {
+//           path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Regular.woff2",
+//           weight: "400",
+//           style: "normal",
+//       },
+//       {
+//           path: "../../public/fonts/yekanbakh/YekanBakhFaNum-SemiBold.woff2",
+//           weight: "600",
+//           style: "normal",
+//       },
+//       {
+//           path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Bold.woff2",
+//           weight: "700",
+//           style: "normal",
+//       },
+//       {
+//           path: "../../public/fonts/yekanbakh/YekanBakhFaNum-Black.woff2",
+//           weight: "900",
+//           style: "normal",
+//       },
+//   ],
+//   variable: "--font-yekanbakh",
+// });
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
+    <html dir="rtl" className={`${figtree.variable}`}>
       <body className="flex flex-col min-h-screen font-bold uppercase">
         <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          HEADER
+          دوره معماری ری اکت
         </header>
         <div className="flex-1 flex  justify-center items-center">
           {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from "next/image";
-
+import { Colors } from "@/app/_components/colors";
 export default function Home() {
   return (
     <>
-      <div className="text-5xl ">React Application Architecture</div>
+      <Colors />
     </>
   );
 }

1faea04a64c83fe0970b463bd325ea0ff74b6ac3
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2026-06-24 18:10:10 +0330

    make Doc of ButtonComponent

--- src/app/_components/button/button.types.ts
+++ src/app/_components/button/button.types.ts
@@ -3,9 +3,11 @@ import { ComponentBase } from "../types/component-base.type";
 import { LoadingBehavior } from "../types/loading-behavior.type";
 
 export type ButtonShape = "default" | "wide" | "full" | "square";
-export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & ComponentBase & LoadingBehavior & { 
+export type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  ComponentBase &
+  LoadingBehavior & {
     isOutline?: boolean;
     shape?: ButtonShape;
     isLink?: boolean;
-    animatedIcon?:boolean;
-};
+    animatedIcon?: boolean;
+  };

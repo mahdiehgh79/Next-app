a0fea8461843190a593527e8a769ca2ace41e48f
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2026-06-30 17:43:23 +0330

    add BadgeComponent

--- src/app/(courses)/_component/course-card.tsx
+++ src/app/(courses)/_component/course-card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { CourseSummary } from "@/types/course-summary.interface";
 import Link from "next/link";
+import { Badge } from "@/app/_components/badge";
 
 export type CourseCardProps = CourseSummary & {};
 
@@ -26,8 +27,8 @@ export const CourseCard: React.FC<CourseCardProps> = ({
         />
       </figure>
       <div className="mt-2 flex gap-2 font-semibold dark:text-info px-3 py-2">
-        {recordStatus}
-        {level}
+        <Badge variant="info">{recordStatus}</Badge>
+        <Badge variant="accent">{level}</Badge>
       </div>
       <div className="card-body">
         <Link href={`/course/${slug}`} className="card-title">
@@ -35,7 +36,7 @@ export const CourseCard: React.FC<CourseCardProps> = ({
         </Link>
         <p>{subTitle}</p>
         <div className="flex items-center justify-between">
-          {duration}
+          <Badge variant="warning">{duration}</Badge>
 
           {basePrice}
         </div>

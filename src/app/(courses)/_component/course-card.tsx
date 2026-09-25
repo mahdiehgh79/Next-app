b46d40c25a66fc9d9d11181d02c1af6be675a56d
mahdiehgh79 <mahdiehghasemi79@yahoo.com>
2026-09-26 01:06:24 +0330

    add PriceComponent

--- src/app/(courses)/_component/course-card.tsx
+++ src/app/(courses)/_component/course-card.tsx
@@ -4,6 +4,7 @@ import { CourseSummary } from "@/types/course-summary.interface";
 import Link from "next/link";
 import { Badge } from "@/app/_components/badge";
 import { IconArrowLeftFill, IconClock } from "@/app/_components/icons/icons";
+import { Price } from "@/app/_components/price/price";
 
 export type CourseCardProps = CourseSummary & {};
 
@@ -36,14 +37,14 @@ export const CourseCard: React.FC<CourseCardProps> = ({
           {title}
         </Link>
         <p>{subTitle}</p>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between mt-3">
           <Badge variant="warning">
             <IconClock width={16} height={16} />
 
             {duration}
           </Badge>
 
-          {basePrice}
+          <Price price={basePrice} />
         </div>
       </div>
       <Link
